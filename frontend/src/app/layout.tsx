16762002dbd98031ac4c52a51ed5cdc1720f2e6d
MargotHuet <margothuet4@gmail.com>
2024-12-06 12:11:58 +0100

    fix build error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import React from "react";
+import React, { Suspense } from "react";
 import '@/fontawesomeconfig';
 import ClientLayout from "@/components/ClientLayout"; 
 
-
 export const metadata: Metadata = {
   title: "Quantum Camp",
-  description: "Learn quantum computing the easy way",
+  description: "Apprennez l'informatique quantique facilement.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body style={{ fontFamily: 'Inter, sans-serif' }}>
-        <ClientLayout>{children}</ClientLayout>
+        <ClientLayout>
+          <Suspense fallback={<div>Chargement...</div>}>
+            {children}
+          </Suspense>
+        </ClientLayout>
       </body>
     </html>
   );
