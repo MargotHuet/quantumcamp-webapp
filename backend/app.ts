5b3b06056400cd52f7abc5acf9ac8d622587dec8
MargotHuet <margothuet4@gmail.com>
2025-01-10 14:29:18 +0100

    https

--- backend/app.ts
+++ backend/app.ts
@@ -24,7 +24,7 @@ const app = express();
 
 // Activer CORS
 app.use(cors({
-  origin: ['http://localhost:3020', 'http://188.165.238.74:3020'],
+  origin: ['https://localhost:3020', 'https://188.165.238.74:3020'],
   credentials: true,             
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
