3ac4b523628880a3c5560f12d527098a6e052970
MargotHuet <margothuet4@gmail.com>
2025-01-17 15:38:22 +0100

    removed a path

--- backend/app.ts
+++ backend/app.ts
@@ -40,7 +40,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chapters', chaptersRouter);
 app.use('/answers', answersRouter);
