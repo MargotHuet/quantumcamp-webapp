56c10acba8884c23b05b237d02817e91306872e7
MargotHuet <margothuet4@gmail.com>
2025-01-17 14:14:43 +0100

    add path

--- backend/app.ts
+++ backend/app.ts
@@ -24,7 +24,7 @@ const app = express();
 
 // Activer CORS
 app.use(cors({
-  origin: ['http://localhost:3020', 'http://188.165.238.74:3020'],
+  origin: ['http://localhost:3020', 'http://188.165.238.74:3020', 'http://quantumcamp.adaschool.fr'],
   credentials: true,             
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
